c75be460f4a27c8040cf3119bb8c464380c7bcee
rachelkiiii <114251753+rachelkiiii@users.noreply.github.com>
2023-01-27 04:51:27 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/*
+      PIC 10A Homework 1, number.cpp
+      Author: Rachel Ki
+      UID: 005590842
+      Discussion Section: 2C
+      Date: 01/20/2023
+*/
+
+#include <iostream>
+using namespace std;
+
+//Problem 2
+int main() {
+    //The function reverses a three digit number and sums each digit
+    cout << "Input a three-digit number (100 - 999):" << endl;
+    
+    int num1;
+    int num2;
+    cin >> num1;
+    
+    num2 = num1 % 100;
+    if (num1 % 10 == 0) {
+        if (num1 % 100 == 0) {
+            cout << "The reverse of the number is " << num1 / 100 << "." << endl;
+        } else {
+            cout << "The reverse of the number is " << (num2 / 10) << (num1 / 100) << "." << endl;
+        }
+    } else {
+        cout << "The reverse of the number is " << (num2 % 10) << (num2 / 10) << (num1 / 100) << "." << endl;
+    }
+    
+    cout << "The sum of the digit of the number is " << (num2 % 10) + (num2 / 10) + (num1 / 100) << "." << endl;
+    return 0;
+}
+
+
+
+
